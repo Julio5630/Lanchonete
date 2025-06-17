597584a048a51cf3c6d65f6b231b9d3eeb4e3e80
Julio Ferreira <juliocesarofficial9@gmail.com>
2025-06-17 15:58:43 -0300

    mudacas

--- atendimento.c
+++ atendimento.c
@@ -14,6 +14,10 @@ typedef struct {
 ItemCardapio cardapio[10];
 int totalItensCardapio = 0;
 
+void limparTela(){
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+}
+
 
 void inicializarCardapio() {
     // Inicializa alguns itens no cardápio
@@ -64,28 +68,25 @@ int main() {
 
         // Loop do Menu Principal
         while (opcaoMenu != 0) {
-            printf("\n====================================================  \n");
-            printf("|                 MENU PRINCIPAL                   |\n");
+             printf("\n====================================================\n");
+            printf("|                   MENU PRINCIPAL                 |\n");
             printf("====================================================\n");
-            printf("| 1 - Atendimento ao Cliente                       |\n");
-            printf("| 2 - Vendas                                       |\n");
+            printf("| 1 - Fazer Pedido (Atendimento ao Cliente)        |\n");
+            printf("| 2 - Gerenciar Vendas                             |\n");
             printf("| 0 - Sair do Sistema                              |\n");
             printf("----------------------------------------------------\n");
-            printf("Escolha uma opcao: ");
             scanf("%d", &opcaoMenu);
 
             switch (opcaoMenu) {
                 case 1: // Menu de Atendimento
                     opcaoAtendimento = -1; 
                     while (opcaoAtendimento != 0) {
+                        limparTela();
                         printf("\n====================================================\n");
-                        printf("|           ATENDIMENTO AO CLIENTE                 |\n");
+                        printf("|              FAZER PEDIDO                          |\n");
                         printf("====================================================\n");
-                        printf("| 1 - Pagamento de Boletos                         |\n");
-                        printf("| 2 - Historico de Atendimento (Em manutencao!!)   |\n");
-                        printf("| 3 - Recarga de Celular                           |\n");
-                        printf("| 4 - Deposito Bancario                            |\n");
-                        printf("| 5 - Saque                                        |\n");
+                        printf("| 1 - Registrar Novo Pedido                        |\n");
+                        printf("| 2 - Exibir Cardápio                              |\n");
                         printf("| 0 - Voltar ao Menu Principal                     |\n");
                         printf("----------------------------------------------------\n");
                         printf("Escolha uma opcao: ");
@@ -97,15 +98,6 @@ int main() {
                                 break;
                             case 2:
                                 printf("\nOpcao 2: Historico de Atendimento\n");
-                                break;
-                            case 3:
-                                printf("\nOpcao 3: Recarga de Celular\n");
-                                break;
-                            case 4:
-                                printf("\nOpcao 4: Deposito Bancario\n");
-                                break;
-                            case 5:
-                                printf("\nOpcao 5: Saque\n");
                                 break;
                             case 0:
                                 printf("\nVoltando ao Menu Principal...\n");
@@ -120,11 +112,9 @@ int main() {
                     opcaoVendas = -1;
                     while (opcaoVendas != 0) {
                         printf("\n====================================================\n");
-                        printf("|                     VENDAS                       |\n");
+                        printf("|                  GERENCIAR VENDAS                |\n");
                         printf("====================================================\n");
-                        printf("| 1 - Registrar Venda de Aposta                    |\n");
-                        printf("| 2 - Consulta de Resultados (Em manutencao!!)     |\n");
-                        printf("| 3 - Pagamento de Premios                         |\n");
+                        printf("| 1 - Visualizar Todas as Vendas                   |\n");
                         printf("| 0 - Voltar ao Menu Principal                     |\n");
                         printf("----------------------------------------------------\n");
                         printf("Escolha uma opcao: ");
@@ -133,12 +123,6 @@ int main() {
                         switch (opcaoVendas) {
                             case 1:
                                 printf("\nOpcao 1: Registrar Venda de Aposta\n");
-                                break;
-                            case 2:
-                                printf("\nOpcao 2: Consulta de Resultados\n");
-                                break;
-                            case 3:
-                                printf("\nOpcao 3: Pagamento de Premios\n");
                                 break;
                             case 0:
                                 printf("\nVoltando ao Menu Principal...\n");
