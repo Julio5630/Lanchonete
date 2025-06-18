a5b5fbb8e652f1fdf12efdca20aa3dff6f13acfe
Julio Ferreira <juliocesarofficial9@gmail.com>
2025-06-18 12:20:35 -0300

    Update atendimento.c

--- atendimento.c
+++ atendimento.c
@@ -18,32 +18,50 @@ void limparTela(){
     printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 }
 
+/*
+Vai Salgadão (Pastel de carne com queijo derretendo)
+
+
+
+*/
+
+
 
 void inicializarCardapio() {
-    cardapio[0] = (ItemCardapio){1, "X-Safadao (Carne, queijo, bacon, molho              )", 25.00};
-    cardapio[1] = (ItemCardapio){2, "X-Laricao (Carne, queijo, alface, tomate            )", 12.50};
-    cardapio[2] = (ItemCardapio){3, "X-Treme Treme (Carne, queijo, barbecue              )", 7.00};
-    cardapio[3] = (ItemCardapio){4, "Pastel do Véi Preguiça (Massa, carne, queijo        )", 18.00};
-    cardapio[4] = (ItemCardapio){5, "Empadão do Soneca (Massa, frango, requeijão         )", 20.00};
+    //  Salgados Fritos
+    cardapio[0] = (ItemCardapio){1, "Pastel de Carne (Massa crocante com carne moída temperada)", 8.50};
+    cardapio[1] = (ItemCardapio){2, "Pastel de Queijo (Queijo derretido na massa frita)", 7.50};
+    cardapio[2] = (ItemCardapio){3, "Coxinha (Massa de batata com frango desfiado)", 6.00};
+    cardapio[3] = (ItemCardapio){4, "Kibe (Trigo, carne moída e hortelã)", 5.50};
+    cardapio[4] = (ItemCardapio){5, "Bolinha de Queijo (Massa crocante com queijo cremoso)", 6.50};
+    cardapio[5] = (ItemCardapio){6, "Risole de Presunto e Queijo", 5.00};
+    cardapio[6] = (ItemCardapio){7, "Enroladinho de Salsicha", 4.50};
 
-    cardapio[5] = (ItemCardapio){6, "X-Bomba Atomica (Duplo, queijo, bacon, alface       )", 30.00};
-    cardapio[6] = (ItemCardapio){7, "X-Termonuke (Picante, cheddar, cebola, picles       )", 28.00};
-    cardapio[7] = (ItemCardapio){8, "X-Ditador (Premium, queijo, bacon, cebola, maionese )", 27.50};
-    cardapio[8] = (ItemCardapio){9, "X-Revolucao (Vegano, queijo, rúcula, tomate, molho  )", 26.00};
+    //  Sanduíches
+    cardapio[7] = (ItemCardapio){8, "Misto Quente (Pão de forma com queijo e presunto gratinado)", 10.00};
+    cardapio[8] = (ItemCardapio){9, "Sanduíche Natural (Pão integral com frango, cenoura e requeijão)", 12.00};
+    cardapio[9] = (ItemCardapio){10, "Bauru (Pão francês com rosbife, queijo derretido e tomate)", 15.00};
+    cardapio[10] = (ItemCardapio){11, "Cachorro-Quente (Pão com salsicha, purê, batata palha e molhos)", 14.00};
 
-    cardapio[9] = (ItemCardapio){10, "Bomba de Chocolate (Brownie, ganache, chocolate    )", 15.00};
-    cardapio[10] = (ItemCardapio){11, "Torta Termonuclear (Chocolate, doce de leite,     )", 18.00};
-    cardapio[11] = (ItemCardapio){12, "Tortadela (Massa, brigadeiro, beijinho, granulados)", 17.00};
+    //  Salgados Assados
+    cardapio[11] = (ItemCardapio){12, "Empada de Frango (Massa folhada com frango e catupiry)", 7.00};
+    cardapio[12] = (ItemCardapio){13, "Esfiha de Carne (Massa aberta com carne moída temperada)", 5.00};
+    cardapio[13] = (ItemCardapio){14, "Pão de Queijo (Tradicional mineiro, quentinho)", 4.00};
+    cardapio[14] = (ItemCardapio){15, "Torta de Frango (Massa crocante com recheio cremoso)", 9.00};
 
-    totalItensCardapio = 12;
+    //  Porções
+    cardapio[15] = (ItemCardapio){16, "Batata Frita (Porção crocante com sal)", 18.00};
+    cardapio[16] = (ItemCardapio){17, "Batata Frita com Cheddar e Bacon", 25.00};
+    cardapio[17] = (ItemCardapio){18, "Mandioquinha Frita", 20.00};
+    cardapio[18] = (ItemCardapio){19, "Isca de Frango (400g com molho barbecue)", 28.00};
+
+    totalItensCardapio = 19;
 }
-
-
 
 
 void exibirCardapio() {
     printf("\n====================================================\n");
-    printf("|         CARDAPIO LANCHONETE KIM LANCHUN          |\n");
+    printf("|       CARDAPIO LANCHONETE WESLEY SALGADAO        |\n");
     printf("====================================================\n");
     printf("| ID | NOME DO ITEM                 | PRECO (R$)   |\n");
     printf("----------------------------------------------------\n");
@@ -52,6 +70,9 @@ void exibirCardapio() {
     }
     printf("====================================================\n");
 }
+
+
+
 
 
 int main() {
@@ -66,8 +87,8 @@ int main() {
 
     // Tela de Login
     printf("====================================================\n");
-    printf("|                 LANCHONETE MEU                   |\n");
-    printf("|               GRELHADO FAVORITO                  |\n");
+    printf("|                    LANCHONETE                    |\n");
+    printf("|                 WESLEY SALGADAO                  |\n");
     printf("====================================================\n");
     printf("\nLogin de acesso: ");+
     scanf("%s", login);
